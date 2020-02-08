7a604905fc6240cee45f90644459315807480964
Qi Zhang <singularitti@outlook.com>
2020-02-08 03:11:45 -0500

    Add extension to `Base.round`

--- config/startup.jl
+++ config/startup.jl
@@ -37,4 +37,5 @@ else
     using LinearAlgebra
     # From https://discourse.julialang.org/t/how-to-pass-multiple-arguments-to-a-function-using/29117/3
     →(args, f) = f(args...)
+    Base.round(x::AbstractMatrix, digits::Int = 15) = round.(x, digits = digits)
 end
