57f1860004600a40e1d8f5cda920cc3774dabb2a
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-15 00:50:44 -0400

    Remove ENV "FONTCONFIG_PATH"

--- config/startup.jl
+++ config/startup.jl
@@ -2,7 +2,6 @@
 let
     ROOT = abspath(Sys.BINDIR, "..")
     ENV["PATH"] = "$(Sys.BINDIR):$(ENV["PATH"])"
-    ENV["FONTCONFIG_PATH"] = joinpath(ROOT, "etc", "fonts")
     ENV["TK_LIBRARY"] = "/System/Library/Frameworks/Tk.framework/Versions/8.5/Resources/Scripts"
     ENV["JULIA_EDITOR"] = "code"
     ENV["MPLBACKEND"] = "MacOSX"
